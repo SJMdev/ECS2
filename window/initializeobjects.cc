b5d8fb79af4eee5f2a5314064475a2605ff13692
sjmdev <sjorsmallon@gmail.com>
2019-02-17 21:16:22 +0100

    cat's back. TODO: fix reading in .obj files.'

--- window/initializeobjects.cc
+++ window/initializeobjects.cc
@@ -22,16 +22,17 @@ void Window::initializeObjects(vector<ObjectFilePaths>  &objectFilePaths)
         object.rotationMatrix.toIdentity();
 
 	    //object.translationMatrix.translateSelf(0,0,-3);
-        object.scaleMatrix.scaleSelf(0.05f);
-        object.modelMatrix = object.rotationMatrix * object.scaleMatrix;
-		object.modelMatrix = object.translationMatrix * object.modelMatrix; 
+        object.scaleMatrix.scaleSelf(0.2f);
+        object.translationMatrix.translateSelf(0, 0, -5.0f);
+        object.modelMatrix = object.translationMatrix * object.rotationMatrix * object.scaleMatrix;
+        SDL_Log("object modelmatrix : %s", object.modelMatrix.toString().c_str());
 	  			
 
         d_objects.push_back(object);    
     }
     
     
-    d_objects.at(1).translationMatrix.translateSelf(0.8,0,0);
+ //    d_objects.at(1).translationMatrix.translateSelf(0.8,0,0);
     //d_objects.at(1).rotationMatrix.rotateSelf(90,0,0);
     
     for (auto &object: d_objects)
